a76326e45e06695788adac47b4d2c293e9393320
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-04-24 17:07:10 +0000

    fetching data to an object

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,52 @@
 <template>
-  <div class="home">
-    Hello world
-  </div>
+  <main v-if="!loading">
+    Show data
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+      <img :src="loadingImage" class="w-24 m-auto" alt="" />
+    </div>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
+
+import axios from 'axios';
 
 export default {
   name: 'Home',
   components: {
     
+  },
+  data()
+  {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      status: {},
+      countries: [],
+      loadingImage: require('../assets/hourglass.gif'),
+    }
+  },
+  methods: {
+    async fetchData()
+    {
+      const res = await axios.get('https://api.covid19api.com/summary');
+      const data = await res.data;
+      return data;
+    }
+  },
+  async created()
+  {
+    const data = await this.fetchData();
+    
+    this.dataDate = data.Date;
+    this.status = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
   }
+  
 }
 </script>
