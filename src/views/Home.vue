635b452194d7d2f8b324215628adeb23aabe1520
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-04-24 18:53:01 +0000

    select country and get Its data shown in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <main v-if="!loading">
     <Title :text="title" :dataDate="dataDate" />
     <Boxes :status="status" />
+    <countrySelect @get-country="getCountryData" :countries="countries" />
   </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -16,12 +17,14 @@
 import axios from 'axios';
 import Title from "@/components/Title.vue";
 import Boxes from '@/components/Boxes.vue';
+import countrySelect from '@/components/countrySelect.vue';
 
 export default {
   name: 'Home',
   components: {
     Title,
-    Boxes,   
+    Boxes,
+    countrySelect,   
   },
   data()
   {
@@ -40,6 +43,11 @@ export default {
       const res = await axios.get('https://api.covid19api.com/summary');
       const data = await res.data;
       return data;
+    },
+    getCountryData(country)
+    {
+      this.status = country;
+      this.title = country.Country;
     }
   },
   async created()
